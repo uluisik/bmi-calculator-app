02100475229bd6fa19b293231e61bec24b845439
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 13:39:55 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,83 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [image, setImage] = useState("");
+  const [message, setMessage] = useState("");
+
+  let calcBmi = (event) => {
+    event.preventDefault();
+
+    if (weight === 0 || height === 0) {
+      alert("please type weight and height");
+    } else {
+      let bmi = weight / (height * height);
+      setBmi(bmi.toFixed());
+
+      if (bmi <= 18) {
+        setMessage("you are weak");
+        setImage("https://i.ytimg.com/vi/okoB9frBGIw/hqdefault.jpg");
+      } else if (bmi <= 25 && bmi >= 18) {
+        setMessage("you are normal");
+        setImage(
+          "https://galeri.uludagsozluk.com/33/kurtlar-vadisi-pusu-kazim_42709.jpg"
+        );
+      } else if (bmi <= 30 && bmi >= 25) {
+        setMessage("you are fat");
+        setImage(
+          "https://galeri13.uludagsozluk.com/713/kurtlar-vadisi-abidin_2162358.jpg"
+        );
+      } else if (bmi >= 30 && bmi <= 35) {
+        setMessage("you are obese");
+        setImage("https://i.ytimg.com/vi/IPbyySh9ROM/hqdefault.jpg");
+      } else if (bmi >= 35) {
+        setMessage("morbid obesity");
+      }
+    }
+  };
+  let reload = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h2 className="center">Bmi Calculator</h2>
+        <form onSubmit={calcBmi}>
+          <div>
+            <label>Weight (kg)</label>
+            <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+          </div>
+          <div>
+            <label>Height (cm)</label>
+            <input value={height} onChange={(e) => setHeight(e.target.value)} />
+          </div>
+          <div>
+            <button className="btn" type="submit" onClick={() => calcBmi()}>
+              Submit
+            </button>
+            <button
+              className="btn btn-outline"
+              type="submit"
+              onClick={() => reload()}
+            >
+              Delete Informations
+            </button>
+          </div>
+        </form>
+        <div className="center">
+          <h3>Your Bmi is : {bmi}</h3>
+          <p>{message}</p>
+        </div>
+        <div className="img-container">
+          <img src={Image} alt="kvk" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
